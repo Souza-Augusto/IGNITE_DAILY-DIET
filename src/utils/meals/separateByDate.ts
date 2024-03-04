898985c6615adced44ef0dac7b6f15f810de9e29
Augusto Souza <souzaaugusto164@gmail.com>
2024-03-04 11:11:09 -0300

    feat: data structuring functions passed to the separateByDates function.

--- src/utils/meals/separateByDate.ts
+++ src/utils/meals/separateByDate.ts
@@ -1,7 +1,7 @@
 import { mealDTO } from 'src/dtos/mealDTO';
 
 type Props = {
-  title: string 
+  title: string;
   data: mealDTO[];
 };
 
@@ -33,5 +33,49 @@ export function SeparateByDate(data: mealDTO[]) {
     }
   });
 
-  return arrayseparate;
+  const converttedDates = arrayseparate.map((item) => {
+    const parts = item.title.split('/');
+    const formattedTitle = Number(`${parts[2]}${parts[1]}${parts[0]}`);
+
+    return {
+      ...item,
+      title: formattedTitle,
+    };
+  });
+
+  const sortDatesDescending = converttedDates.sort((a, b) => {
+    const dateA = a.title;
+    const dateB = b.title;
+    return dateB - dateA;
+  });
+
+  const sortHoursDescending = sortDatesDescending.map((item) => {
+    const sortDatesDescending = item.data.sort((a, b) => {
+      const hourA = a.hour.split(':');
+      const hourB = b.hour.split(':');
+
+      return (
+        Number(hourB[0]) - Number(hourA[0]) ||
+        Number(hourB[1]) - Number(hourA[1])
+      );
+    });
+    return {
+      title: item.title,
+      data: sortDatesDescending,
+    };
+  });
+
+  const formattedttedDates = sortHoursDescending.map((item) => {
+    const parts = String(item.title);
+    const year = parts.substring(2, 4);
+    const month = parts.substring(4, 6);
+    const day = parts.substring(6, 8);
+
+    return {
+      ...item,
+      title: `${day}.${month}.${year}`,
+    };
+  });
+
+  return formattedttedDates;
 }
