b9b4877a2720a7baa5bbe5693503f50f296b6b04
Augusto Souza <souzaaugusto164@gmail.com>
2024-03-07 10:39:51 -0300

    feat:create CountOffDietMeals function.

---
+++ src/utils/meals/countOffDietMeals.ts
@@ -0,0 +1,24 @@
+import { mealDTO } from 'src/dtos/mealDTO';
+
+type Props = {
+  title: string;
+  data: mealDTO[];
+}[];
+
+export function CountOffDietMeals(data: Props) {
+  try {
+    let count = 0;
+
+    data.forEach((group) => {
+      group.data.forEach((meal) => {
+        if (!meal.healthy) {
+          count++;
+        }
+      });
+    });
+
+    return count;
+  } catch (error) {
+    throw error;
+  }
+}
