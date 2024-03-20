f54ba481a99d9cf9a6bbfa7d90a7708e5bdb7da6
Augusto Souza <souzaaugusto164@gmail.com>
2024-03-20 10:39:54 -0300

    test:fix useNavigation mock.

--- src/views/components/Header/back-button/view-model.test.ts
+++ src/views/components/Header/back-button/view-model.test.ts
@@ -2,14 +2,16 @@ import { act, renderHook } from '@__tests__/utils/customRenderItem';
 import { useBackButtonViewModel } from './view-model';
 import { useNavigation } from '@react-navigation/native';
 
-jest.mock('@react-navigation/native');
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: jest.fn(),
+}));
 
 beforeEach(() => {
   jest.clearAllMocks();
 });
 
 const mockedGoBack = jest.fn();
-(useNavigation as jest.Mock).mockReturnValue({
+jest.mocked(useNavigation).mockReturnValue({
   goBack: mockedGoBack,
 });
 
